52a96c4643a61b797671aa4d95544c19d59a9661
nitcops <nicolasdimartino23@gmail.com>
2026-04-24 15:18:48 +0200

    Update Deploy.sh

--- scripts/Deploy.sh
+++ scripts/Deploy.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+# Mise à jour des paquets
+sudo apt update && apt upgrade
 
+# Installation de Docker et du plugin Compose
+sudo apt install -y docker.io docker-compose-v2
+
+# On s'assure que Docker démarre bien
+sudo systemctl enable --now docker
+
+# Optionnel : pour ne pas avoir à taper 'sudo' à chaque fois (nécessite de se reconnecter)
+# sudo usermod -aG docker $USER
