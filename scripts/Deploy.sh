25240827e4d66b6132b0237423f22e96e8b6a6b3
nitcops <nicolasdimartino23@gmail.com>
2026-04-26 18:37:19 +0200

    Update Deploy.sh

--- scripts/Deploy.sh
+++ scripts/Deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# Paquet élévation de privilège
 apt install sudo -y
 # Mise à jour des paquets
-sudo apt update && apt upgrade
+sudo apt update && sudo apt upgrade -y
 
-# Installation de Docker et du plugin Compose
+# Installation de Docker et du plugin Compose + paquet de test et d'édition
 sudo apt install -y docker.io docker-compose curl vim
 
 # On s'assure que Docker démarre bien
